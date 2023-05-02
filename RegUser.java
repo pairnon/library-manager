c28e4d3f6b4d65da69ce42924b3066ba9e4c5755
tdtcs <fhollis983@gmail.com>
2023-05-02 14:04:34 -0700

    added book sorting

--- RegUser.java
+++ RegUser.java
@@ -22,4 +22,83 @@ public class RegUser extends User {
         return loans;
     }
 
+    public ArrayList<Book> getBooksByAuthorAsc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getAuthor().compareTo(books.get(back).getAuthor()) < 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort descending
+    public ArrayList<Book> getBooksByAuthorDesc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getAuthor().compareTo(books.get(back).getAuthor()) > 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort ascending
+    public ArrayList<Book> getBooksByTitleAsc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getTitle().compareTo(books.get(back).getTitle()) < 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort descending
+    public ArrayList<Book> getBooksByTitleDesc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getTitle().compareTo(books.get(back).getTitle()) > 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
 }
