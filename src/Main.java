b9efa5c81e8fe9d5d415d0a9e7df9df6f4d6f49d
tdtcs <fhollis983@gmail.com>
2023-05-02 09:17:50 -0700

    added login loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        Superuser admin = new Superuser("admin", 0, "root");
+        
+        boolean loggedIn = false;
+        User identity;
+        
+        while(!loggedIn) {
+            System.out.println("Admin Login");
+            System.out.println("default is admin, root");
+
+
+            System.out.println("Username:");
+            String username = scanner.nextLine();
+            
+            System.out.println("Password:");
+            String password = scanner.nextLine();
+
+            // TODO: Authentication
+
+            loggedIn = true;
+        }
+
     }
 
 }
