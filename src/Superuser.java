aadfd0b41107632d10c497ac5d11a5dbd37bf67a
tdtcs <fhollis983@gmail.com>
2023-05-01 19:20:04 -0700

    created class for admins

--- src/Superuser.java
+++ src/Superuser.java
@@ -1,3 +1,16 @@
+import java.security.NoSuchAlgorithmException;
+
 public class Superuser extends User {
+
+    private PasswordHash password;
+
+    public Superuser(String name, int id, String password) throws NoSuchAlgorithmException {
+        super(name, id);
+        this.password = new PasswordHash(SHA256.getDigest(password));
+    }
+
+    public PasswordHash getPassword() {
+        return password;
+    }
     
 }
