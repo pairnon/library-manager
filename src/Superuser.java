b76c6f03d2df1cdc9701af6d6eac3210bc27c87b
tdtcs <fhollis983@gmail.com>
2023-05-01 19:30:05 -0700

    removed throws declarations

--- src/Superuser.java
+++ src/Superuser.java
@@ -4,9 +4,13 @@ public class Superuser extends User {
 
     private PasswordHash password;
 
-    public Superuser(String name, int id, String password) throws NoSuchAlgorithmException {
+    public Superuser(String name, int id, String password) {
         super(name, id);
-        this.password = new PasswordHash(SHA256.getDigest(password));
+        try {
+            this.password = new PasswordHash(SHA256.getDigest(password));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     public PasswordHash getPassword() {
