7a4168966c48dfd71643dbaabb57a6079d49b21d
tdtcs <fhollis983@gmail.com>
2023-05-02 14:01:58 -0700

    removed loans

--- src/User.java
+++ src/User.java
@@ -4,21 +4,10 @@ public class User {
     
     private String name;
     private int id;
-    private ArrayList<Loan> loans;
 
     public User(String name, int id) {
         this.name = name;
         this.id = id;
-        this.loans = new ArrayList<Loan>();
-    }
-
-    public User(String name, int id, ArrayList<Loan> loans) {
-        this.name = name;
-        this.id = id;
-        this.loans = new ArrayList<Loan>();
-        for(Loan loan : loans) {
-            this.loans.add(loan);
-        }
     }
 
     public String getName() {
@@ -29,20 +18,9 @@ public class User {
         return id;
     }
 
-    public void addLoan(Loan loan) {
-        loans.add(loan);
-    }
-
-    public ArrayList<Loan> getLoans() {
-        return loans;
-    }
-
     public String toString() {
         String out = "";
-        out += "Name: " + name + " | ID: " + id + "\nLoans\n";
-        for(Loan loan : loans) {
-            out += loan + "\n";
-        }
+        out += "Name: " + name + " | ID: " + id;
         return out;
     }
 
