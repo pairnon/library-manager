058ab2371faa17dbee411c56caf16d347315cdfc
tdtcs <fhollis983@gmail.com>
2023-05-02 14:24:43 -0700

    surrounded scanner with try

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Superuser admin = new Superuser("admin", 0, "root");
-        
-        boolean loggedIn = admin.getLogInStatus();
+        try (Scanner scanner = new Scanner(System.in)) {
+            Superuser admin = new Superuser("admin", 0, "root");
+            
+            boolean loggedIn = admin.getLogInStatus();
 
-        while(!loggedIn) {
-            System.out.println("Admin Login");
-            System.out.println("default password is 'root'");
+            while(!loggedIn) {
+                System.out.println("Admin Login");
+                System.out.println("default password is 'root'");
 
-            System.out.println("Password:");
-            String password = scanner.nextLine();
+                System.out.println("Password:");
+                String password = scanner.nextLine();
 
-            loggedIn = admin.authenticate(password);
+                loggedIn = admin.authenticate(password);
+            }
         }
-
         System.out.println("login success");
 
     }
