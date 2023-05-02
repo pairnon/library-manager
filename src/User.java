8e534b5bc1cab13fe088b5898dfb5ad9b9e6d4f0
tdtcs <fhollis983@gmail.com>
2023-05-02 09:07:43 -0700

    added loans

--- src/User.java
+++ src/User.java
@@ -9,6 +9,16 @@ public class User {
     public User(String name, int id) {
         this.name = name;
         this.id = id;
+        this.loans = new ArrayList<Loan>();
+    }
+
+    public User(String name, int id, ArrayList<Loan> loans) {
+        this.name = name;
+        this.id = id;
+        this.loans = new ArrayList<Loan>();
+        for(Loan loan : loans) {
+            this.loans.add(loan);
+        }
     }
 
     public String getName() {
@@ -25,6 +35,15 @@ public class User {
 
     public ArrayList<Loan> getLoans() {
         return loans;
+    }
+
+    public String toString() {
+        String out = "";
+        out += "Name: " + name + " | ID: " + id + "\nLoans\n";
+        for(Loan loan : loans) {
+            out += loan + "\n";
+        }
+        return out;
     }
 
 }
