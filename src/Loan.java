cf70d7ffc7978464b0dd32f521220dace105a887
tdtcs <fhollis983@gmail.com>
2023-05-02 00:04:19 -0700

    base Loan class

--- src/Loan.java
+++ src/Loan.java
@@ -1,3 +1,27 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Loan {
     
+    Book book;
+    Date issueDate;
+    Date dueDate;
+
+    public Loan(Book book) {
+        this.book = book;
+        this.issueDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, 5);
+        this.dueDate = calendar.getTime();
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String toString() {
+        return (book + " | Issued: " + issueDate + " | Due: " + dueDate);
+    }
+
 }
