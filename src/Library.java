f466bcca2f93a089c799f9c99a5e0c7e00804c66
tdtcs <fhollis983@gmail.com>
2023-05-01 19:34:25 -0700

    base Library class

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Library {
-    
+
+    private ArrayList<Book> books;
+
+    public Library(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
 }
