9f165c67fb8181ae84d29d3c517052e103d62e69
tdtcs <fhollis983@gmail.com>
2023-05-02 14:22:59 -0700

    added authentication

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Superuser admin = new Superuser("admin", 0, "root");
         
-        boolean loggedIn = false;
-        User identity;
-        
+        boolean loggedIn = admin.getLogInStatus();
+
         while(!loggedIn) {
             System.out.println("Admin Login");
-            System.out.println("default login is admin, root");
+            System.out.println("default password is 'root'");
 
-
-            System.out.println("Username:");
-            String username = scanner.nextLine();
-            
             System.out.println("Password:");
             String password = scanner.nextLine();
 
-            // TODO: Authentication
-
-            loggedIn = true;
+            loggedIn = admin.authenticate(password);
         }
+
+        System.out.println("login success");
 
     }
 
