07b36b5fcba53d2ba88bdbc8ab5b02e565dc35a4
tdtcs <fhollis983@gmail.com>
2023-05-01 19:14:41 -0700

    created class for passwords

---
+++ src/PasswordHash.java
@@ -0,0 +1,13 @@
+public class PasswordHash {
+
+    private String passwordHash;
+
+    public PasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+}
