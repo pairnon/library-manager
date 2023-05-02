5d48ec2b3ed5c00884a0a71c59b2e0118f1039b5
tdtcs <fhollis983@gmail.com>
2023-05-02 08:48:27 -0700

    added users

--- src/Library.java
+++ src/Library.java
@@ -3,9 +3,15 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> books;
+    private ArrayList<User> users;
 
     public Library(ArrayList<Book> books) {
         this.books = books;
+    }
+
+    public Library(ArrayList<Book> books, ArrayList<User> users) {
+        this.books = books;
+        this.users = users;
     }
 
     public Library(Book[] books) {
@@ -21,10 +27,18 @@ public class Library {
         return books;
     }
 
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
     public String toString() {
         String out = "";
         for(Book book : books) {
-            out += book + "\n";
+            out += book + " | ";
+        }
+        out += "\n";
+        for(User user : users) {
+            out += user + " | ";
         }
         return out;
     }
