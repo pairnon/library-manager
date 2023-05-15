b8c7164121a5a41c191a4ad01aa75e96c4d1611d
pairnon <fhollis983@gmail.com>
2023-05-15 10:03:47 -0700

    refactored

--- src/Instantiator.java
+++ src/Instantiator.java
@@ -12,48 +12,33 @@ public class Instantiator {
             books.add(book);
         }
 
-        ArrayList<Loan> loans1 = new ArrayList<Loan>();
-        ArrayList<Loan> loans2 = new ArrayList<Loan>();
-        ArrayList<Loan> loans3 = new ArrayList<Loan>();
-        ArrayList<Loan> loans4 = new ArrayList<Loan>();
-        ArrayList<Loan> loans5 = new ArrayList<Loan>();
-
-        for(int i = 0; i < 10; i++) {
-            int index = (int)(Math.random() * 199);
-            Loan loan = new Loan(books.get(index));
-            loans1.add(loan);
-
-            index = (int)(Math.random() * 199);
-            loan = new Loan(books.get(index));
-            loans2.add(loan);
-
-            index = (int)(Math.random() * 199);
-            loan = new Loan(books.get(index));
-            loans3.add(loan);
-
-            index = (int)(Math.random() * 199);
-            loan = new Loan(books.get(index));
-            loans4.add(loan);
-
-            index = (int)(Math.random() * 199);
-            loan = new Loan(books.get(index));
-            loans5.add(loan);
-        }
-
-        RegUser regUser1 = new RegUser("Alice", 1, loans1);
-        RegUser regUser2 = new RegUser("Bob", 2, loans2);
-        RegUser regUser3 = new RegUser("Carol", 3, loans3);
-        RegUser regUser4 = new RegUser("Dave", 4, loans4);
-        RegUser regUser5 = new RegUser("Eve", 5, loans5);
-
-        regUsers.add(regUser1);
-        regUsers.add(regUser2);
-        regUsers.add(regUser3);
-        regUsers.add(regUser4);
-        regUsers.add(regUser5);
+        regUsers = instantiateRegUsers(books);
 
         Library lib = new Library(books, admin, regUsers);
 
         return lib;
+    }
+
+    public static ArrayList<RegUser> instantiateRegUsers(ArrayList<Book> books) {
+
+        String[] names = {
+            "Alice", "Bob", "Carol", "Dave", "Eve", "Frank", "Grace", "Hank", "Isabel", "Jack",
+            "Karen", "Liam", "Mandy", "Nancy", "Oliver", "Pamela", "Quentin", "Rachel", "Samuel",
+            "Tina", "Ursula", "Victor", "Wendy", "Xander", "Yvonne", "Zack"
+        };
+        
+        ArrayList<RegUser> out = new ArrayList<RegUser>();
+        
+        for(int i = 0; i < names.length; i++) {
+            ArrayList<Loan> loans = new ArrayList<Loan>();
+            for(int j = 0; j < 10; j++) {
+                int index = (int)(Math.random() * 199);
+                Loan loan = new Loan(books.get(index));
+                loans.add(loan);
+            }
+            out.add(new RegUser(names[i], i+1, loans));
+        }
+
+        return out;
     }
 }
