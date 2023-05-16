a94b6e64cc5c2750db118752b6a0cb9118478c33
pairnon <fhollis983@gmail.com>
2023-05-16 09:07:13 -0700

    moved methods to parent class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class User {
     
     private String name;
@@ -14,6 +16,86 @@ public class User {
 
     public int getId() {
         return id;
+    }
+
+    // Insertion sort ascending
+    public ArrayList<Book> getBooksByAuthorAsc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getAuthor().compareTo(books.get(back).getAuthor()) < 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort descending
+    public ArrayList<Book> getBooksByAuthorDesc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getAuthor().compareTo(books.get(back).getAuthor()) > 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort ascending
+    public ArrayList<Book> getBooksByTitleAsc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getTitle().compareTo(books.get(back).getTitle()) < 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
+    }
+
+    // Insertion sort descending
+    public ArrayList<Book> getBooksByTitleDesc(Library library) {
+        ArrayList<Book> books = library.getBooks();
+
+        for(int i = 1; i < books.size(); i++) {
+            Book book = books.get(i);
+            int back = i - 1;
+
+            while(back >= 0 && book.getTitle().compareTo(books.get(back).getTitle()) > 0) {
+                books.set(back+1, books.get(back));
+                back--;
+            }
+
+            books.set(back + 1, book);
+
+        }
+
+        return books;
     }
 
     public String toString() {
