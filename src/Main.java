54fae5d4d64361fa9f145f6ea2e7abc78d9f894c
pairnon <fhollis983@gmail.com>
2023-05-16 22:21:31 -0700

    added comments

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Superuser admin = new Superuser("admin", 0, "root");
 
+        // initialize the library, which requires a Superuser to be constructed
+        Superuser admin = new Superuser("admin", 0, "root");
         Library lib = Instantiator.instantiateLibrary(admin);
 
         System.out.println("");
 
         Scanner scanner = new Scanner(System.in);
 
+        // prompt for regUser or Superuser login
         while(true) {
             System.out.println("LOGIN\n1 - user login\n2 - admin login\nx - exit");
             System.out.println("Action:");
@@ -31,7 +33,5 @@ public class Main {
 
         scanner.close();
     }
-
-    
 
 }
