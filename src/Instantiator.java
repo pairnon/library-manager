7364b1ab9350ccc506de657d45e794100d23d2bd
pairnon <fhollis983@gmail.com>
2023-05-16 22:11:29 -0700

    prettified and added comments

--- src/Instantiator.java
+++ src/Instantiator.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 
+// class used solely for method organization
 public class Instantiator {
+
+    // initialize a library with all attributes
     public static Library instantiateLibrary(Superuser admin) {
         ArrayList<RegUser> regUsers = new ArrayList<RegUser>();
         ArrayList<Book> books = new ArrayList<Book>();
 
-        for (int i = 0; i < 200; i++) {
+        // initialize books
+        int numBooks = 200;
+        for (int i = 0; i < numBooks; i++) {
             String title = "Book " + i;
             String author = "Author " + i;
             Book book = new Book(title, author);
@@ -19,8 +24,10 @@ public class Instantiator {
         return lib;
     }
 
+    // instantiateLibrary helper method
     public static ArrayList<RegUser> instantiateRegUsers(ArrayList<Book> books) {
 
+        // list of names each starting with a unique letter
         String[] names = {
             "Alice", "Bob", "Carol", "Dave", "Eve", "Frank", "Grace", "Hank", "Isabel", "Jack",
             "Karen", "Liam", "Mandy", "Nancy", "Oliver", "Pamela", "Quentin", "Rachel", "Samuel",
@@ -29,9 +36,11 @@ public class Instantiator {
         
         ArrayList<RegUser> out = new ArrayList<RegUser>();
         
+        // initialize a new regUser for each name, each with random loans
+        int numUserLoans = 3;
         for(int i = 0; i < names.length; i++) {
             ArrayList<Loan> loans = new ArrayList<Loan>();
-            for(int j = 0; j < 3; j++) {
+            for(int j = 0; j < numUserLoans; j++) {
                 int index = (int)(Math.random() * 199);
                 Loan loan = new Loan(books.get(index));
                 loans.add(loan);
